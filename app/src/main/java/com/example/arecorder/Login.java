9c1cc9ade53a1887f9b185edf72837a951992e7e
raj-gaurav <rajgauravraj97@gmail.com>
2020-02-09 11:43:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/arecorder/Login.java
@@ -0,0 +1,90 @@
+package com.example.arecorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class Login extends AppCompatActivity {
+
+    TextView txt,fpass;
+    TextInputLayout username, password;
+    Button login;
+    String value;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        init();
+        value=getIntent().getExtras().getString("key");
+        if(value.equals("1"))
+        {
+            txt.setText("ADMIN LOGIN");
+
+        }
+        else if(value.equals("2"))
+        {
+            txt.setText("TEACHER LOGIN");
+        }
+        else if(value.equals("3")){
+            txt.setText("STUDENT LOGIN");
+
+        }
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(value.equals("1")){
+                    adminAction();
+                }
+            }
+        });
+    }
+
+    public void init(){
+        txt=findViewById(R.id.txt);
+        fpass=findViewById(R.id.fpass);
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        login=findViewById(R.id.btnLogin);
+    }
+
+    public void adminAction()
+    {
+        int a=0,b=0;
+        String user=username.getEditText().getText().toString().trim();
+        String pass=password.getEditText().getText().toString().trim();
+        if(user.isEmpty()){
+            username.setError("Field can't be empty");
+        }
+        else if(user.equals("rajgauravraj97") ){
+            username.setError(null);
+            a=1;
+        }
+        else{
+            username.setError("Invalid Username");
+        }
+        if(pass.isEmpty()){
+            password.setError("Field can't be empty");
+        }
+        else if(pass.equals("123456789")){
+            password.setError(null);
+            b=1;
+        }
+        else{
+            password.setError("Invalid Password");
+        }
+
+        if(a==1 && b==1)
+            Toast.makeText(getApplicationContext(),"ALL OK!!!!",Toast.LENGTH_SHORT).show();
+    }
+
+
+}
