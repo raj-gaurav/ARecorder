0c33ca02741b343aadda5fa1cffad279be9c8bcf
raj-gaurav <rajgauravraj97@gmail.com>
2020-02-21 17:40:15 +0530

    1.0.3.0 logout functionality

--- app/src/main/java/com/example/arecorder/TeacherHomePage.java
+++ app/src/main/java/com/example/arecorder/TeacherHomePage.java
@@ -10,6 +10,8 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -106,6 +108,25 @@ public class TeacherHomePage extends AppCompatActivity {
 
 
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId()==R.id.logout)
+        {
+            Intent i=new Intent(getApplicationContext(),Login.class);
+            startActivity(i);
+            finish();
+        }
+
+        return true;
     }
 
     public void init(){
